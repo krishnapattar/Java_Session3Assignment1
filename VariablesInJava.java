f1ef38c0a0055490a2abee5fc9c339e389d9cad1
krishnapattar <krishnapattar140@gmail.com>
2017-07-10 00:00:51 +0530

    Add files via upload

---
+++ VariablesInJava.java
@@ -0,0 +1,42 @@
+
+ public class VariablesInJava {
+ 
+	/*
+	 * Below variable is INSTANCE VARIABLE as it is outside any method and it is
+	 * not using STATIC modifier with it. 
+	 * 	 */
+	int instanceField;
+ 
+	/*
+	 * Below variable is STATIC variable as it is outside any method and it is
+	 * using STATIC modifier with it. 
+	 */
+	static String staticField;
+ 
+	public void method() {
+		/*
+		 * Below variable is LOCAL VARIABLE as it is defined inside method in
+		 * class.
+		 */
+		final String localVariable = "Initial Value";
+		System.out.println(localVariable);
+	}
+ 
+	public static void main(String args[]) {
+		VariablesInJava obj = new VariablesInJava();
+ 
+		/*
+		 * Instance variable can only be accessed by Object of the class only as below.
+		 */
+		System.out.println(obj.instanceField);
+ 
+		/*
+		 * Static field can be accessed in two way.
+		 * 1- Via Object of the class
+		 * 2- Via CLASS name
+		 */
+		System.out.println(obj.staticField);
+		System.out.println(VariablesInJava.staticField);
+                System.out.println(new VariablesInJava().instanceField);
+	}
+}
